6dbca79806b8d5524dc3c5853d21d9ccdb589c37
Yaroslav <lixometr@gmail.com>
2022-01-05 13:28:30 +0200

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: ["./public/**/*.html", "./src/**/*.vue"],
+  purge: ["./public/**/*.html", "./src/**/*.vue", 'node_modules/vue-tailwind/dist/*.js'],
   variants: {
     extend: {
       opacity: ["disabled"],
       cursor: ["disabled"],
     },
   },
+  mode: 'jit',
   theme: {
     container: {
       center: true,
